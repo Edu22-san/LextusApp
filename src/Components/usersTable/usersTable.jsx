1299825c11dee9ce16b7d9efe14a0badccf8f8ff
Raulo <rauleduardovigil@gmail.com>
2023-12-01 12:26:43 -0600

    page view users

--- src/Components/usersTable/usersTable.jsx
+++ src/Components/usersTable/usersTable.jsx
@@ -11,6 +11,26 @@ const Userstable = () => {
 
   return (
     <>
+    <div className="w-full flex flex-row items-end justify-between mb-[2.5rem]">
+      <div className="colum1">
+         <h1 className="text-5xl font-semibold text-white">Customer</h1>
+         <p className="text-white text-sm font-thin">565 registered customers</p>
+      </div>
+      <button className="border-2 border-solid border-white rounded-2xl text-white pt-2 pb-2 pl-15 pr-15 w-[9rem]">Add new</button>
+    </div>
+    <div className="w-full mb-[2rem]">
+      <div className="flex flex-row items-baseline">
+         <p className="text-white text-sm font-thin mr-12"> All customers</p>
+         <p className="text-white text-sm font-thin mr-12"> Last 30 days</p>
+         <p className="text-white text-sm font-thin mr-12">Last 6 months</p>
+         <select className="bg-transparent text-white outline-none" name="" id="">
+          <option value="">Last year</option>
+          <option value="">Option 1</option>
+          <option value="">Option 2</option>
+         </select>
+      </div>
+      <input type="search" className="w-[20rem] mt-2 p-2 border border-gray-300 rounded-3xl outline-none" placeholder="Search..." />
+    </div>
       <div className="relative overflow-x-auto">
         <table className="w-full text-sm text-left rtl:text-right text-white">
           <thead className=" text-white uppercase table-enc">
@@ -42,15 +62,15 @@ const Userstable = () => {
             <tr className="bg-white text-blue-txt ">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium whitespace-nowrap  rounded-l-3xl"
+                className="px-6 py-3 font-medium whitespace-nowrap  rounded-l-3xl"
               >
                 Name
               </th>
-              <td className="px-6 py-4">Visa U</td>
-              <td className="px-6 py-4">01/01/23</td>
-              <td className="px-6 py-4">10%</td>
-              <td className="px-6 py-4">View CheckList</td>
-              <td className="px-6 py-4 rounded-r-3xl ">
+              <td className="px-6 py-3">Visa U</td>
+              <td className="px-6 py-3">01/01/23</td>
+              <td className="px-6 py-3">10%</td>
+              <td className="px-6 py-3">View CheckList</td>
+              <td className="px-6 py-3 rounded-r-3xl ">
                 <div className="container-options">
                    <div className="dropdown mr-4">
                      <i className="fa-solid fa-chevron-down icon-verde cursor-pointer" onClick={toggleDropdown}></i>
@@ -71,15 +91,15 @@ const Userstable = () => {
             <tr className="bg-white text-blue-txt ">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium whitespace-nowrap  rounded-l-3xl"
+                className="px-6 py-3 font-medium whitespace-nowrap  rounded-l-3xl"
               >
                 Name
               </th>
-              <td className="px-6 py-4">Visa U</td>
-              <td className="px-6 py-4">01/01/23</td>
-              <td className="px-6 py-4">10%</td>
-              <td className="px-6 py-4">View CheckList</td>
-              <td className="px-6 py-4 rounded-r-3xl">
+              <td className="px-6 py-3">Visa U</td>
+              <td className="px-6 py-3">01/01/23</td>
+              <td className="px-6 py-3">10%</td>
+              <td className="px-6 py-3">View CheckList</td>
+              <td className="px-6 py-3 rounded-r-3xl">
                 <i class="fa-solid fa-chevron-down icon-verde mr-4"></i>
                 <i class="fa-solid fa-magnifying-glass icon-buscar"></i>
               </td>
@@ -90,15 +110,15 @@ const Userstable = () => {
             <tr className="bg-white text-blue-txt ">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium whitespace-nowrap  rounded-l-3xl"
+                className="px-6 py-3 font-medium whitespace-nowrap  rounded-l-3xl"
               >
                 Name
               </th>
-              <td className="px-6 py-4">Visa U</td>
-              <td className="px-6 py-4">01/01/23</td>
-              <td className="px-6 py-4">10%</td>
-              <td className="px-6 py-4">View CheckList</td>
-              <td className="px-6 py-4 rounded-r-3xl">
+              <td className="px-6 py-3">Visa U</td>
+              <td className="px-6 py-3">01/01/23</td>
+              <td className="px-6 py-3">10%</td>
+              <td className="px-6 py-3">View CheckList</td>
+              <td className="px-6 py-3 rounded-r-3xl">
                 <i class="fa-solid fa-chevron-down icon-verde mr-4"></i>
                 <i class="fa-solid fa-magnifying-glass icon-buscar"></i>
               </td>
@@ -109,15 +129,15 @@ const Userstable = () => {
             <tr className="bg-white text-blue-txt ">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium whitespace-nowrap  rounded-l-3xl"
+                className="px-6 py-3 font-medium whitespace-nowrap  rounded-l-3xl"
               >
                 Name
               </th>
-              <td className="px-6 py-4">Visa U</td>
-              <td className="px-6 py-4">01/01/23</td>
-              <td className="px-6 py-4">10%</td>
-              <td className="px-6 py-4">View CheckList</td>
-              <td className="px-6 py-4 rounded-r-3xl">
+              <td className="px-6 py-3">Visa U</td>
+              <td className="px-6 py-3">01/01/23</td>
+              <td className="px-6 py-3">10%</td>
+              <td className="px-6 py-3">View CheckList</td>
+              <td className="px-6 py-3 rounded-r-3xl">
                 <i class="fa-solid fa-chevron-down icon-verde mr-4"></i>
                 <i class="fa-solid fa-magnifying-glass icon-buscar"></i>
               </td>
