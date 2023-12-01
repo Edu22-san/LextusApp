fc20fe51f9e9003b222dbdaffa7773fd55879723
Raulo <rauleduardovigil@gmail.com>
2023-12-01 11:21:43 -0600

    user-logo

--- src/Components/menu/menu.jsx
+++ src/Components/menu/menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "./Logo-lextus.png";
-import User from "../../assets/img/user.png";
+import User from "./user.png";
 import "./menuStyle.css"; 
 
 const Menu = () => {
