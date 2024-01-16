bf5b179071af4dd4b1999799826358470d3d2eb0
Raulo <rauleduardovigil@gmail.com>
2024-01-16 17:03:11 -0600

    texto en ingles state dashboard customer

--- src/pages/dashboardCustomer/dashboardCustomer.jsx
+++ src/pages/dashboardCustomer/dashboardCustomer.jsx
@@ -57,10 +57,10 @@ const DashboardCustomer = () => {
         </div>
         <div className="w-[90%] mt-[1rem] flex flex-row justify-center md:justify-end lg:justify-end">
           <p className="flex flex-row items-center justify-center text-white text-[20px]">
-            <span className="w-[20px] h-[20px] bg-bg-verde mr-[10px] rounded-[3px]"></span>Entregado
+            <span className="w-[20px] h-[20px] bg-bg-verde mr-[10px] rounded-[3px]"></span>Delivered
           </p>
           <p className="flex flex-row items-center justify-center text-white text-[20px] ml-[2rem] ">
-            <span className="w-[20px] h-[20px] bg-bg-rojo mr-[10px] rounded-[3px]"></span>Pendiente
+            <span className="w-[20px] h-[20px] bg-bg-rojo mr-[10px] rounded-[3px]"></span>Pending
           </p>
         </div>
 
