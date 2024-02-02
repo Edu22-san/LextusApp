8d12fccc76497e3f1907cec47bd579d35b26e42f
Raulo <rauleduardovigil@gmail.com>
2024-02-02 12:13:59 -0600

    payment plan content

--- src/pages/formNewCustomer/formNewCustomer.jsx
+++ src/pages/formNewCustomer/formNewCustomer.jsx
@@ -18,11 +18,22 @@ const checklistPlan = [
   { name: "Parole" },
 ];
 
+const checklistPaymentPlan = [
+  { name: "Payment Plan 1" },
+  { name: "Payment Plan 2" },
+  { name: "Payment Plan 3" },
+  { name: "Payment Plan 4" },
+  { name: "Payment Plan 5" },
+  { name: "Payment Plan 6" },
+];
+
 const FormNewCustomer = () => {
   const [firstName, setFirstName] = useState("");
   const [selectedImage, setSelectedImage] = useState(null);
   const [selected, setSelected] = useState(checklistPlan[-1]);
-  const tabs = ["Asign Checklist", "Asign Payment plan", "Send Password"];
+  const [selectedPaymentPlan, setSelectedPaymentPlan] = useState(checklistPaymentPlan[-1]);
+
+  const tabs = ["Asign Checklist", "Payment plan", "Send Password"];
 
   const onUpload = (documentId) => {
     toast.current.show({
@@ -45,6 +56,16 @@ const FormNewCustomer = () => {
     { label: "Document name | pending | 01/01/2024", id: 6 },
   ];
 
+  const documentsChecklistData = [
+    { label: "Document name ", id: 1 },
+    { label: "Document name ", id: 2 },
+    { label: "Document name ", id: 3 },
+    { label: "Document name ", id: 4 },
+    { label: "Document name ", id: 5 },
+    { label: "Document name ", id: 6 },
+  ];
+
+ 
   return (
     <>
       <div className="min-w-full p-[2rem] md:p-[4rem] lg:p-[4rem]">
@@ -132,7 +153,7 @@ const FormNewCustomer = () => {
                 ))}
               </Tab.List>
 
-              <Tab.Panels className="w-full mt-[2rem] md:mt-[0] lg:mt-[0]  h-[70vh] md:h-[50vh] lg:h-[50vh]">
+              <Tab.Panels className="w-full mt-[2rem] md:mt-[0] lg:mt-[0]  h-[70vh] md:h-[60vh] lg:h-[60vh]">
                 <Tab.Panel
                   className={classNames(
                     "rounded-xl bg-white p-3",
@@ -141,11 +162,7 @@ const FormNewCustomer = () => {
                 >
                   <div className="w-full flex flex-row items-center justify-between border-b-2 border-solid border-gray-400 mb-4 h-[15%]">
                     <div className="flex flex-row items-center justify-start ">
-                      <img
-                        src={IconChecklist}
-                        alt=""
-                        className="mr-1 w-[20px]"
-                      />
+                      <img src={IconChecklist} className="mr-1 w-[30px]"/>
                       <p className="text-xl font-bold text-blue-txt">
                         New Relative
                       </p>
@@ -154,89 +171,156 @@ const FormNewCustomer = () => {
                       Save
                     </button>
                   </div>
-                  <div className="flex flex-col h-[80%]">
-                    <div className="w-full md:w-[60%]  lg:w-[60%] h-[40%] border-[2px] border-solid border-gray-300 rounded-[14px] p-[7px]">
-                      <div className="w-full h-full overflow-y-auto">
-                        <RadioGroup
-                          value={selected}
-                          onChange={setSelected}
-                          className="pr-[1rem]"
-                        >
-                          <div className="space-y-2">
-                            {checklistPlan.map((plan) => (
-                              <RadioGroup.Option
-                                key={plan.name}
-                                value={plan}
-                                className={({ active, checked }) =>
-                                  `${
-                                    active
-                                      ? "ring-2 ring-white/60 ring-offset-2 ring-offset-sky-300"
-                                      : ""
-                                  }
+                  <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 h-[80%]">
+                    <div className="p-4 col-span-1 h-[20vh] md:h-[43vh] lg:h-[43vh] row-span-1 border-[2px] border-solid border-gray-300 rounded-[14px] p-[7px] overflow-y-auto">
+                      <p className="text-blue-txt text-[18px] mb-[5px]">
+                        Select Matter:
+                      </p>
+                      <RadioGroup
+                        value={selected}
+                        onChange={setSelected}
+                        className=""
+                      >
+                        <div className="space-y-2">
+                          {checklistPlan.map((plan) => (
+                            <RadioGroup.Option
+                              key={plan.name}
+                              value={plan}
+                              className={({ active, checked }) =>
+                                `${
+                                  active
+                                    ? "ring-2 ring-white/60 ring-offset-2 ring-offset-sky-300"
+                                    : ""
+                                }
                                   ${
                                     checked
                                       ? "bg-sky-900/75 text-white"
-                                      : "bg-white border-[2px] border-solid border-gray-300"
+                                      : "bg-white border-[2px] border-solid border-gray-300 w-full"
                                   }
                                     relative flex cursor-pointer rounded-lg px-5 py-2 shadow-md focus:outline-none`
-                                }
-                              >
-                                {({ active, checked }) => (
-                                  <>
-                                    <div className="flex w-full items-center justify-between">
-                                      <div className="flex items-center">
-                                        <div className="text-sm">
-                                          <RadioGroup.Label
-                                            as="p"
-                                            className={`font-medium  ${
-                                              checked
-                                                ? "text-white"
-                                                : "text-gray-900"
-                                            }`}
-                                          >
-                                            {plan.name}
-                                          </RadioGroup.Label>
-                                        </div>
+                              }
+                            >
+                              {({ active, checked }) => (
+                                <>
+                                  <div className="flex w-full items-center justify-between">
+                                    <div className="flex items-center">
+                                      <div className="text-sm">
+                                        <RadioGroup.Label
+                                          as="p"
+                                          className={`font-medium  ${
+                                            checked
+                                              ? "text-white"
+                                              : "text-gray-900"
+                                          }`}
+                                        >
+                                          {plan.name}
+                                        </RadioGroup.Label>
                                       </div>
-                                      {checked && (
-                                        <div className="shrink-0 text-white">
-                                          <CheckIcon className="h-6 w-6" />
-                                          {console.log(
-                                            "Radio button seleccionado:",
-                                            plan.name
-                                          )}
-                                        </div>
-                                      )}
                                     </div>
-                                  </>
-                                )}
-                              </RadioGroup.Option>
-                            ))}
-                          </div>
-                        </RadioGroup>
-                      </div>
+                                    {checked && (
+                                      <div className="shrink-0 text-white">
+                                        <CheckIcon className="h-6 w-6" />
+                                      </div>
+                                    )}
+                                  </div>
+                                </>
+                              )}
+                            </RadioGroup.Option>
+                          ))}
+                        </div>
+                      </RadioGroup>
                     </div>
-                    <div className=" h-[60%] p-[5px] ">
-                      Or Ad hoc Checklist
-                      <div className="container-casc">
-                        <input
-                          type="text"
-                          id="document_name"
-                          class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-blue-txt "
-                          placeholder="Document name"
-                          required
-                        />
-                      </div>
+                    <div className=" p-4 col-span-2 row-span-1 h-[30vh] md:h-[43vh] lg:h-[43vh] p-[5px] border-[2px] border-solid border-gray-300 rounded-[14px] overflow-y-auto">
+                      <p className="text-blue-txt text-[18px] mb-[5px]">
+                        Or Ad hoc Checklist:
+                      </p>
+                      <ul className="list-decimal w-full pr-[5px]">
+                        {documentsChecklistData.map((document) => (
+                          <li key={document.id} className="mb-[12px] ml-[17px]">
+                            <div className="flex flex-row justify-between items-center">
+                              <label className="text-[15px]  md:text-[17px] lg:text-[17px]">
+                                {document.label}
+                              </label>
+                             <input type="date" className="inputselectcustoms"/>
+                            </div>
+                          </li>
+                        ))}
+                      </ul>
                     </div>
                   </div>
                 </Tab.Panel>
                 <Tab.Panel
                   className={classNames(
-                    "rounded-xl bg-white p-3",
+                    "rounded-xl bg-white",
                     "ring-white/60 ring-offset-2 ring-offset-blue-400 h-full focus:outline-none focus:ring-2"
                   )}
                 >
-                  <p>View Payment</p>
+                  <div className="p-4 col-span-1 h-[70vh] md:h-[60vh] lg:h-[60vh] row-span-1 border-[2px] border-solid border-gray-300 rounded-[14px] p-3 overflow-y-auto">
+                  <div className="w-full flex flex-row items-center justify-between border-b-2 border-solid border-gray-400 mb-4 h-[15%]">
+                    <div className="flex flex-row items-center justify-start ">
+                      <img src={IconChecklist} className="mr-1 w-[30px]"/>
+                      <p className="text-xl font-bold text-blue-txt">
+                        Select Payment Plan:
+                      </p>
+                    </div>
+                    <button className="bg-gray-300 text-black rounded-md px-5 py-1 text-base md:text-[15px] font-normal font-manrope">
+                      Save
+                    </button>
+                  </div>
+                      <RadioGroup
+                        value={selectedPaymentPlan}
+                        onChange={setSelectedPaymentPlan}
+                        className=""
+                      >
+                        <div className="space-y-2">
+                          {checklistPaymentPlan.map((plan) => (
+                            <RadioGroup.Option
+                              key={plan.name}
+                              value={plan}
+                              className={({ active, checked }) =>
+                                `${
+                                  active
+                                    ? "ring-2 ring-white/60 ring-offset-2 ring-offset-sky-300"
+                                    : ""
+                                }
+                                  ${
+                                    checked
+                                      ? "bg-sky-900/75 text-white"
+                                      : "bg-white border-[2px] border-solid border-gray-300 w-full"
+                                  }
+                                    relative flex cursor-pointer rounded-lg px-5 py-3 shadow-md focus:outline-none`
+                              }
+                            >
+                              {({ active, checked }) => (
+                                <>
+                                  <div className="flex w-full items-center justify-between">
+                                    <div className="flex items-center">
+                                      <div className="text-sm">
+                                        <RadioGroup.Label
+                                          as="p"
+                                          className={`font-medium  ${
+                                            checked
+                                              ? "text-white"
+                                              : "text-gray-900"
+                                          }`}
+                                        >
+                                          {plan.name}
+                                        </RadioGroup.Label>
+                                      </div>
+                                    </div>
+                                    {checked && (
+                                      <div className="shrink-0 text-white">
+                                        <CheckIcon className="h-6 w-6" />
+                                      </div>
+                                    )}
+                                  </div>
+                                </>
+                              )}
+                            </RadioGroup.Option>
+                          ))}
+                        </div>
+                      </RadioGroup>
+                    </div>
                 </Tab.Panel>
                 <Tab.Panel
                   className={classNames(
@@ -255,7 +339,7 @@ const FormNewCustomer = () => {
           <div className="column2-document">
             <div className="w-full flex flex-row items-center justify-between border-b-2 border-solid border-gray-400 mb-[1px]">
               <div className="flex flex-row items-center justify-start ">
-                <img src={IconChecklist} alt="" className="mr-1 w-[20px]" />
+                <img src={IconChecklist} alt="" className="mr-1 w-[30px]" />
                 <p className="text-xl font-bold text-blue-txt">
                   View CheckList
                 </p>
@@ -264,7 +348,7 @@ const FormNewCustomer = () => {
             <div className="w-full h-full flex flex-col items-start rounded-12 pt-[12px] pb-[12px]">
               <div className="flex flex-row items-center justify-start ">
                 <i class="fa-regular fa-folder-open pr-[5px] text-gray-400 text-3xl"></i>
-                <p className="text-xl font-bold text-white">Document List</p>
+                <p className="text-xl font-bold text-gray-400">Document List</p>
               </div>
               <div className="w-full grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4">
                 <div className="bg-bg-celeste p-[12px] rounded-2xl h-[40vh] shadow-lg ">
